0449b3d874c9d2472927afc59c51dd0dd5506237
jakub <jakubszur@gmail.com>
2019-10-22 22:00:49 +0200

    Added temperature tracking via web server

--- FlaskApp/mainTemplate.py
+++ FlaskApp/mainTemplate.py
@@ -3,14 +3,19 @@
 
 from flask import Flask, render_template
 import datetime
+import w1thermsensor
+
 app = Flask(__name__, template_folder='templates')
 @app.route("/")
 def main():
    now = datetime.datetime.now()
+   sensor=w1thermsensor.W1ThermSensor()
+   temp = sensor.get_temperature()
    timeString = now.strftime("%Y-%m-%d %H:%M")
    templateData = {
-      'title' : 'HELLO!',
-      'time': timeString
+      'title' : 'Pi Temperature Monitor',
+      'time': timeString,
+      'temperature' : temp
       }
    return render_template('main.html', **templateData)
 if __name__ == "__main__":
