fc7466f57bf59f6f8d98bbd1ee083f853a07038e
Jakub Szur <jakubszur@gmail.com>
2019-10-29 22:59:49 +0100

    insert query to insert data to database, add files and foder to ingnore by git

--- temperature.py
+++ temperature.py
@@ -7,14 +7,33 @@ import pymysql
 connection = pymysql.connect(host='localhost',
                              user='root',
                              password='root',
-                             db='db',
+                             db='PiTemperatureda',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-# check temperature in 5 minutes interval
+# check temperature and write to database in interval
 while True:
+        # get temperature from sensor
     sensor = w1thermsensor.W1ThermSensor()
     temp = sensor.get_temperature()
-    now = datetime.datetime.now()
+    # get date
+    now = datetime.now()
+    date = now.strftime('%Y-%m-%d')
+    # get hour
+    hour = now.strftime('%H:%M')
     print(temp)
+
+    try:
+        # write data to database
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO outside_temperature VALUES (%s,%s,%s)"
+            insert_tuple = (str(temp), date, hour)
+            cursor.execute(sql, insert_tuple)
+            print(
+                f"Data saved: temperature: {temp}; date: {date}; time: {hour}")
+            connection.commit()
+
+    finally:
+        connection.close()
+    # check temperature every 5 minutes
     time.sleep(300)
