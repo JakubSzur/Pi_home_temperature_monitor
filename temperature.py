c5218010345800b6b767f995391aa7768c413f8b
Jakub Szur <jakubszur@gmail.com>
2019-10-29 22:39:40 +0100

    added pymsql module and connection to database PiTemperature

--- temperature.py
+++ temperature.py
@@ -1,6 +1,15 @@
 import w1thermsensor
 import time
 import datetime
+import pymysql.cursors
+import pymysql
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 # check temperature in 5 minutes interval
 while True:
