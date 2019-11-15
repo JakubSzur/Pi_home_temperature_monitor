78e3decfca512d7e9f9c9cc6e696a7fc04167e26
Jakub Szur <jakubszur@gmail.com>
2019-11-15 13:56:47 +0100

    Added function to draw plot of

--- chart_generator.py
+++ chart_generator.py
@@ -1,6 +1,8 @@
 import pymysql.cursors
 import pymysql
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import datetime
 
 # connect to database
 connection = pymysql.connect(host='localhost',
@@ -10,10 +12,12 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+
+# function to get data from DB i specified range of time
 def query_to_get_rows(hours, timestamp, value, table):
     # number of rows to get with measurement in timestamp of measurement
     rows = int((hours*60)/timestamp)
-    
+
     # queries to get data from database
     with connection.cursor() as cursor:
         # get outside temperature from defined number of hours
@@ -30,8 +34,50 @@ def query_to_get_rows(hours, timestamp, value, table):
 
     return (query_result_list)
 
+
+# function to convert list with time to readable string format
+# time from query is in the datetime.timedelta format
+def datatime_query(hours, timestamp, value, table):
+    # get data with time from query
+    time_to_convert = query_to_get_rows(hours, timestamp, value, table)
+    # list to collect converted data
+    readable_time = []
+    # convert and add data to list
+    for i in time_to_convert:
+        time_in_seconds = i.seconds
+
+        hours = int(time_in_seconds/3600)
+        hours = str(hours)
+        if len(hours)==1:
+            hours = f'0{hours}'
+        minutes = int((time_in_seconds%3600)/60)
+        minutes = str(minutes)
+        if len(minutes)==1:
+            minutes = f'0{minutes}'
+
+        time = f'{hours}:{minutes}'
+        readable_time.append(time)
+
+    return readable_time
+
+
+# function to draw linear plot
+def draw_linear_plot(xdata, ydata, xlabel, ylabel):
+    plt.plot(xdata, ydata)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    #plt.grid(True, axis='y')
+    plt.grid(True)
+    ax = plt.axes()
+    ax.xaxis.set_major_locator(plt.MaxNLocator(24))
+    plt.xticks(rotation=70)
+    plt.show()
+
+
 if __name__ == "__main__":
 
-    print(query_to_get_rows(24, 'temperature', 'outside_temperature', 5))
-    print(query_to_get_rows(24, 'temperature', 'inside_temperature', 5))
-    print(query_to_get_rows(24, 'humidity', 'inside_humidity', 5))
+    outside_temperature = (query_to_get_rows(24, 5, 'temperature', 'outside_temperature'))
+    time = datatime_query(24, 5, 'hour', 'outside_temperature')
+    #print(query_to_get_rows(24, 'humidity', 'inside_humidity', 5))
+
+    draw_linear_plot(time,outside_temperature, 'time', 'outside temperature')
